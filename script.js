32e9230169017382441052cefd869644cde634d6
Md-Shahzadah-Sattar <148210694+Md-Shahzadah-Sattar@users.noreply.github.com>
2023-12-22 20:13:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-const parseCode = (str) => {
-  // your code here
-};
+function parseCode(encodedString) {
+  // Split the input string by "000" to extract firstName, lastName, and id
+  const [firstName, lastName, id] = encodedString.split("000");
+
+  // Return an object with the extracted values
+  return {
+    firstName: firstName,
+    lastName: lastName,
+    id: id
+  };
+}
+
+// Examples
+console.log(parseCode("John000Doe000123"));
+// Output: { firstName: "John", lastName: "Doe", id: "123" }
+
+console.log(parseCode("michael0smith004331"));
+// Output: { firstName: "michael", lastName: "smith", id: "4331" }
+
+console.log(parseCode("Thomas00LEE0000043"));
+// Output: { firstName: "Thomas", lastName: "LEE", id: "43" }
 
 // Do not change the code below
-const str = prompt("Enter str: ");
-alert(JSON.stringify(parseCode(str)));
+// const str = prompt("Enter str: ");
+// alert(JSON.stringify(parseCode(str)));
